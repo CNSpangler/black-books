3cff1c399e65cfd1da516737ed1c6ede4a072f97
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-21 17:17:33 -0800

    set up homepage, assets, and all files

---
+++ data/books.js
@@ -0,0 +1,74 @@
+const longWay = {
+    id: longWay,
+    title: 'The Long Way to a Small, Angry Planet',
+    author: 'Becky Chambers',
+    image: './assets/longway.jpg',
+    price: 7.95
+};
+
+const calculating = {
+    id: calculating,
+    title: 'The Calculating Stars',
+    author: 'Mary Robinette Kowal',
+    image: './assets/calculating.jpg',
+    price: 8.95
+};
+
+const spaceOpera = {
+    id: spaceOpera,
+    title: 'Space Opera',
+    author: 'Catherynne M. Valente',
+    image: './assets/',
+    price: 10.95
+};
+
+const flying = {
+    id: flying,
+    title: 'The Book of Flying',
+    author: 'Keith Miller',
+    image: './assets/',
+    price: 4.95
+};
+
+const aegypt1 = {
+    id: aegypt1,
+    title: 'The Solitudes',
+    author: 'John Crowley',
+    image: './assets/',
+    price: 6.95
+};
+
+const aegypt2 = {
+    id: aegypt2,
+    title: 'Love & Sleep',
+    author: 'John Crowley',
+    image: './assets/',
+    price: 6.95
+};
+
+const aegypt3 = {
+    id: aegypt3,
+    title: 'Daemonomania',
+    author: 'John Crowley',
+    image: './assets/',
+    price: 6.95
+};
+
+const aegypt4 = {
+    id: aegypt4,
+    title: 'Endless Things',
+    author: 'John Crowley',
+    image: './assets/',
+    price: 6.95
+};
+
+export const books = [
+    longWay,
+    spaceOpera,
+    calculating,
+    flying,
+    aegypt1,
+    aegypt2,
+    aegypt3,
+    aegypt4,
+];
