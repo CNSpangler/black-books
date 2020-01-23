f0a387b06d4bff435224ebee34c390f83529c566
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-22 16:18:18 -0800

    updated to pass test

--- test/example.test.js
+++ test/example.test.js
@@ -17,7 +17,7 @@ const result = longWayLi.outerHTML;
 console.log(result);
 
 test('should convert object to html li output', function(assert) {
-    const expected = '<li id="longWay" class="paperback" title="The Long Way to a Small, Angry Planet"><h3>The Long Way to a Small, Angry Planet</h3><h5>by Becky Chambers</h5><img src="../assets/longWay.jpg" alt="The Long Way to a Small, Angry Planet"><p class="7.95"><button value="longWay">Add</button></p></li>';
+    const expected = '<li id="longWay" class="paperback" title="The Long Way to a Small, Angry Planet"><h3>The Long Way to a Small, Angry Planet</h3><h5>by Becky Chambers</h5><img src="../assets/../assets/longWay.jpg" alt="The Long Way to a Small, Angry Planet"><p class="7.95">$7.95<button value="longWay">Add</button></p></li>';
 
     assert.equal(expected, result);
 });
